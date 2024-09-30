e806936a10aa752e8d9d106894ab8c251ec40bc7
Vinisxx <viniciuseduardo0500@gmail.com>
2024-09-30 06:36:36 -0300

    update the Gray for new format of colors

---
+++ src/main/java/com/codes/vinis/vLogger/color/defaultColors/Gray.java
@@ -0,0 +1,11 @@
+package com.codes.vinis.vLogger.color.defaultColors;
+
+import com.codes.vinis.vLogger.color.Color;
+
+public final class Gray extends Color {
+
+    public Gray() {
+
+        super("Gray", "\u001B[90m");
+    }
+}
