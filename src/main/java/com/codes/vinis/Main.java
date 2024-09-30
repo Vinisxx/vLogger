e8b37b181a86f54ab9df8c698e4f7ae834d82ec9
Vinisxx <viniciuseduardo0500@gmail.com>
2024-09-30 10:22:29 -0300

    tested the new format of LogLevels

--- src/main/java/com/codes/vinis/Main.java
+++ src/main/java/com/codes/vinis/Main.java
@@ -4,44 +4,30 @@ import com.codes.vinis.vLogger.color.defaultColors.DarkRed;
 import com.codes.vinis.vLogger.color.defaultColors.Gray;
 import com.codes.vinis.vLogger.color.defaultColors.Green;
 import com.codes.vinis.vLogger.color.defaultColors.Orange;
+import com.codes.vinis.vLogger.logLevels.Debug;
+import com.codes.vinis.vLogger.logLevels.Error;
+import com.codes.vinis.vLogger.logLevels.Info;
+import com.codes.vinis.vLogger.logLevels.Warn;
 import org.jetbrains.annotations.NotNull;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        @NotNull Gray gray = new Gray();
+        @NotNull Info info = new Info("this method isn't finalized, use the 'otherMethod()'.", new Gray());
 
-        System.out.println(gray);
+        System.out.println(info.getColor().getColor() + info.getMessage());
 
-        @NotNull Green green = new Green();
+        @NotNull Debug debug = new Debug("success in this operation.", new Green());
 
-        System.out.println(green);
+        System.out.println(debug.getColor().getColor() + debug.getMessage());
 
-        @NotNull Orange orange = new Orange();
+        @NotNull Warn warn = new Warn("this process is generating an unwanted bottleneck.", new Orange());
 
-        System.out.println(orange);
+        System.out.println(warn.getColor().getColor() + warn.getMessage());
 
-        @NotNull DarkRed darkRed = new DarkRed();
+        @NotNull Error error = new Error("invalid operation.", new DarkRed());
 
-        System.out.println(darkRed);
-        /*
-        @NotNull Info info = new Info("This is a info");
-
-        System.out.println(info.getMessage());
-
-        @NotNull Debug debug = new Debug("This is a debug");
-
-        System.out.println(debug.getMessage());
-
-        @NotNull Warn warn = new Warn("This is a warn");
-
-        System.out.println(warn.getMessage());
-
-        @NotNull Error error = new Error("This is a error");
-
-        System.out.println(error.getMessage());
-
-         */
+        System.out.println(error.getColor().getColor() + error.getMessage());
     }
 }
