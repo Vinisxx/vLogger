511cd034b16a282c5142d105ebe3c614e884267a
Vinisxx <viniciuseduardo0500@gmail.com>
2024-09-30 06:37:11 -0300

    update the DarkRed for new format of colors

---
+++ src/main/java/com/codes/vinis/vLogger/color/defaultColors/DarkRed.java
@@ -0,0 +1,13 @@
+package com.codes.vinis.vLogger.color.defaultColors;
+
+import com.codes.vinis.vLogger.color.Color;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public final class DarkRed extends Color {
+
+    public DarkRed() {
+
+        super("DarkRed", "\u001B[38;5;88m");
+    }
+}
