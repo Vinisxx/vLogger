bcca2057e60b7122a034f39936fd31c586696619
Vinisxx <viniciuseduardo0500@gmail.com>
2024-09-30 11:05:35 -0300

    updated Green for new model of Color

--- src/main/java/com/codes/vinis/vLogger/color/defaultColors/Green.java
+++ src/main/java/com/codes/vinis/vLogger/color/defaultColors/Green.java
@@ -4,10 +4,53 @@ import com.codes.vinis.vLogger.color.Color;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-public final class Green extends Color {
+public final class Green implements Color {
+
+    private final @NotNull String name;
+
+    private final @NotNull String color;
 
     public Green() {
 
-        super( "Green", "\u001B[32m");
+        this.name = "Green";
+        this.color = "\u001B[32m";
+    }
+
+    @Override
+    public @NotNull String getName() {
+
+        return name;
+    }
+
+    @Override
+    public @NotNull String getColor() {
+
+        return color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        @NotNull Green green = (Green) o;
+        return getName().equals(green.getName()) && getColor().equals(green.getColor());
+    }
+
+    @Override
+    public int hashCode() {
+
+        int result = getName().hashCode();
+        result = 31 * result + getColor().hashCode();
+
+        return result;
+    }
+
+    @Contract(pure = true)
+    @Override
+    public @NotNull String toString() {
+
+        return "{ \"Color\": \"" + getColor() + getName() + "\u001B[0m\" }";
     }
 }
