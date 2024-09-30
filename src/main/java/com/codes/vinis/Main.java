0f54a294706fa4c313153fcef84d1f5338d58abd
Vinisxx <viniciuseduardo0500@gmail.com>
2024-09-30 05:20:32 -0300

    tested the classes Info, Debug, Warn, Error. And now I made a Color class.

--- src/main/java/com/codes/vinis/Main.java
+++ src/main/java/com/codes/vinis/Main.java
@@ -1,9 +1,29 @@
 package com.codes.vinis;
 
+import com.codes.vinis.vLogger.logLevels.Debug;
+import com.codes.vinis.vLogger.logLevels.Error;
+import com.codes.vinis.vLogger.logLevels.Info;
+import com.codes.vinis.vLogger.logLevels.Warn;
+import org.jetbrains.annotations.NotNull;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello guys");
+        @NotNull Info info = new Info("This is a info");
+
+        System.out.println(info.getMessage());
+
+        @NotNull Debug debug = new Debug("This is a debug");
+
+        System.out.println(debug.getMessage());
+
+        @NotNull Warn warn = new Warn("This is a warn");
+
+        System.out.println(warn.getMessage());
+
+        @NotNull Error error = new Error("This is a error");
+
+        System.out.println(error.getMessage());
     }
 }
