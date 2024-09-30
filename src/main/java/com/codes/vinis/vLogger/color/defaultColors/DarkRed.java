cf28d7a4e80e81bb99bae30441a96919701e3efe
Vinisxx <viniciuseduardo0500@gmail.com>
2024-09-30 11:06:18 -0300

    updated DarkRed for new model of Color

--- src/main/java/com/codes/vinis/vLogger/color/defaultColors/DarkRed.java
+++ src/main/java/com/codes/vinis/vLogger/color/defaultColors/DarkRed.java
@@ -4,10 +4,53 @@ import com.codes.vinis.vLogger.color.Color;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-public final class DarkRed extends Color {
+public final class DarkRed implements Color {
+
+    private final @NotNull String name;
+
+    private final @NotNull String color;
 
     public DarkRed() {
 
-        super("DarkRed", "\u001B[38;5;88m");
+        this.name = "DarkRed";
+        this.color = "\u001B[38;5;88m";
+    }
+
+    @Override
+    public @NotNull String getName() {
+
+        return name;
+    }
+
+    @Override
+    public @NotNull String getColor() {
+
+        return color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        @NotNull DarkRed darkRed = (DarkRed) o;
+        return getName().equals(darkRed.getName()) && getColor().equals(darkRed.getColor());
+    }
+
+    @Override
+    public int hashCode() {
+
+        int result = getName().hashCode();
+        result = 31 * result + getColor().hashCode();
+
+        return result;
+    }
+
+    @Contract(pure = true)
+    @Override
+    public @NotNull String toString() {
+
+        return "{ \"Color\": \"" + getColor() + getName() + "\u001B[0m\" }";
     }
 }
