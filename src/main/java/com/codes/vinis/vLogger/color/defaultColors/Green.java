25d35746f26fad1ced0d19daad8ce7c31790d390
Vinisxx <viniciuseduardo0500@gmail.com>
2024-09-30 06:36:46 -0300

    update the Green for new format of colors

---
+++ src/main/java/com/codes/vinis/vLogger/color/defaultColors/Green.java
@@ -0,0 +1,13 @@
+package com.codes.vinis.vLogger.color.defaultColors;
+
+import com.codes.vinis.vLogger.color.Color;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public final class Green extends Color {
+
+    public Green() {
+
+        super( "Green", "\u001B[32m");
+    }
+}
