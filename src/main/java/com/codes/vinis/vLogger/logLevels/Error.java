d156d5a34900be3cfa76f088b458c0451e8ad784
Vinisxx <viniciuseduardo0500@gmail.com>
2024-09-30 09:40:44 -0300

    implements the interface LogLevel

--- src/main/java/com/codes/vinis/vLogger/logLevels/Error.java
+++ src/main/java/com/codes/vinis/vLogger/logLevels/Error.java
@@ -1,26 +1,46 @@
 package com.codes.vinis.vLogger.logLevels;
 
+import com.codes.vinis.vLogger.color.Color;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.StringJoiner;
 
-public class Error {
+
+public class Error implements LogLevel{
+
 
     private @NotNull String message;
 
-    public Error(@NotNull String message) {
+    private @NotNull Color color;
+
+    public Error(@NotNull String message, @NotNull Color color) {
 
         this.message = message;
+        this.color = color;
     }
 
+    @Override
     public @NotNull String getMessage() {
 
         return message;
     }
 
+    @Override
     public void setMessage(@NotNull String message) {
 
         this.message = message;
+    }
+
+    @Override
+    public @NotNull Color getColor() {
+
+        return color;
+    }
+
+    @Override
+    public void setColor(@NotNull Color color) {
+
+        this.color = color;
     }
 
     @Override
@@ -30,20 +50,22 @@ public class Error {
         if (o == null || getClass() != o.getClass()) return false;
 
         @NotNull Error error = (Error) o;
-        return message.equals(error.message);
+        return getMessage().equals(error.getMessage()) && getColor().equals(error.getColor());
     }
 
     @Override
     public int hashCode() {
 
-        return message.hashCode();
+        int result = getMessage().hashCode();
+        result = 31 * result + getColor().hashCode();
+        return result;
     }
 
     @Override
     public String toString() {
-
         return new StringJoiner(", ", Error.class.getSimpleName() + "[", "]")
                 .add("message='" + message + "'")
+                .add("color=" + color)
                 .toString();
     }
 }
