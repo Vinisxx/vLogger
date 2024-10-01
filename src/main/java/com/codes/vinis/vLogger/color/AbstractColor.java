8bb0d3ba793c4a6f80da24a5903642ea361401ed
Vinisxx <viniciuseduardo0500@gmail.com>
2024-10-01 11:02:17 -0300

    up for use new system of pre-configuration

---
+++ src/main/java/com/codes/vinis/vLogger/color/AbstractColor.java
@@ -0,0 +1,56 @@
+package com.codes.vinis.vLogger.color;
+
+import com.codes.vinis.vLogger.interfaces.Color;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public class AbstractColor implements Color {
+
+    private final @NotNull String name;
+
+    private final @NotNull String color;
+
+    public AbstractColor(@NotNull String name, @NotNull String color) {
+
+        this.name = name;
+        this.color = color;
+    }
+
+    @Override
+    public @NotNull String getName() {
+
+        return name;
+    }
+
+    @Override
+    public @NotNull String getColor() {
+
+        return color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        @NotNull AbstractColor color = (AbstractColor) o;
+        return getName().equals(color.getName()) && getColor().equals(color.getColor());
+    }
+
+    @Override
+    public int hashCode() {
+
+        int result = getName().hashCode();
+        result = 31 * result + getColor().hashCode();
+
+        return result;
+    }
+
+    @Contract(pure = true)
+    @Override
+    public @NotNull String toString() {
+
+        return "{ \"Color\": \"" + getColor() + getName() + "\u001B[0m\" }";
+    }
+}
