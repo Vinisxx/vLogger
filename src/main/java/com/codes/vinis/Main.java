34f94a20a07d3d68d002184ee48fbadb930bcdeb
Vinisxx <viniciuseduardo0500@gmail.com>
2024-10-01 11:06:20 -0300

    tested the new structure of pre-configurations

--- src/main/java/com/codes/vinis/Main.java
+++ src/main/java/com/codes/vinis/Main.java
@@ -1,32 +1,28 @@
 package com.codes.vinis;
 
-import com.codes.vinis.vLogger.color.defaultColors.DarkRed;
-import com.codes.vinis.vLogger.color.defaultColors.Gray;
-import com.codes.vinis.vLogger.color.defaultColors.Green;
-import com.codes.vinis.vLogger.color.defaultColors.Orange;
-import com.codes.vinis.vLogger.logLevels.Debug;
-import com.codes.vinis.vLogger.logLevels.Error;
-import com.codes.vinis.vLogger.logLevels.Info;
-import com.codes.vinis.vLogger.logLevels.Warn;
+import com.codes.vinis.vLogger.logLevels.defaultLevels.Debug;
+import com.codes.vinis.vLogger.logLevels.defaultLevels.Error;
+import com.codes.vinis.vLogger.logLevels.defaultLevels.Info;
+import com.codes.vinis.vLogger.logLevels.defaultLevels.Warn;
 import org.jetbrains.annotations.NotNull;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        @NotNull Info info = new Info("this method isn't finalized, use the 'otherMethod()'.", new Gray());
+        @NotNull Info info = new Info("this method isn't finalized, use the 'otherMethod()'.");
 
         System.out.println(info.getColor().getColor() + info.getMessage());
 
-        @NotNull Debug debug = new Debug("success in this operation.", new Green());
+        @NotNull Debug debug = new Debug("success in this operation.");
 
         System.out.println(debug.getColor().getColor() + debug.getMessage());
 
-        @NotNull Warn warn = new Warn("this process is generating an unwanted bottleneck.", new Orange());
+        @NotNull Warn warn = new Warn("this process is generating an unwanted bottleneck.");
 
         System.out.println(warn.getColor().getColor() + warn.getMessage());
 
-        @NotNull Error error = new Error("invalid operation.", new DarkRed());
+        @NotNull Error error = new Error("invalid operation.");
 
         System.out.println(error.getColor().getColor() + error.getMessage());
     }
