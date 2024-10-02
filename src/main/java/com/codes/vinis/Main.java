cc41cbc6ffd456ceb85fff41f4a99e5baefc06b1
Vinisxx <viniciuseduardo0500@gmail.com>
2024-10-02 06:41:19 -0300

    tested the new model

--- src/main/java/com/codes/vinis/Main.java
+++ src/main/java/com/codes/vinis/Main.java
@@ -1,29 +1,16 @@
 package com.codes.vinis;
 
-import com.codes.vinis.vLogger.logLevels.defaultLevels.Debug;
-import com.codes.vinis.vLogger.logLevels.defaultLevels.Error;
-import com.codes.vinis.vLogger.logLevels.defaultLevels.Info;
-import com.codes.vinis.vLogger.logLevels.defaultLevels.Warn;
+import com.codes.vinis.vLogger.Logger;
+import com.codes.vinis.vLogger.formatter.SimpleFormatter;
+import com.codes.vinis.vLogger.handler.ConsoleHandler;
 import org.jetbrains.annotations.NotNull;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        @NotNull Info info = new Info("this method isn't finalized, use the 'otherMethod()'.");
+        @NotNull Logger logger = new Logger(Main.class, new ConsoleHandler(new SimpleFormatter()));
 
-        System.out.println(info.getColor().getColor() + info.getMessage());
-
-        @NotNull Debug debug = new Debug("success in this operation.");
-
-        System.out.println(debug.getColor().getColor() + debug.getMessage());
-
-        @NotNull Warn warn = new Warn("this process is generating an unwanted bottleneck.");
-
-        System.out.println(warn.getColor().getColor() + warn.getMessage());
-
-        @NotNull Error error = new Error("invalid operation.");
-
-        System.out.println(error.getColor().getColor() + error.getMessage());
+        logger.error("hello world");
     }
 }
