1ebbd54dfe062bad2df738dd23aabd9d3649236c
Vinisxx <viniciuseduardo0500@gmail.com>
2024-10-03 16:43:46 -0300

    example for usage

--- src/main/java/com/codes/vinis/Main.java
+++ src/main/java/com/codes/vinis/Main.java
@@ -1,16 +1,31 @@
 package com.codes.vinis;
 
 import com.codes.vinis.vLogger.Logger;
+import com.codes.vinis.vLogger.formatter.JsonFormatter;
 import com.codes.vinis.vLogger.formatter.SimpleFormatter;
 import com.codes.vinis.vLogger.handler.ConsoleHandler;
+import com.codes.vinis.vLogger.handler.FileHandler;
+import com.codes.vinis.vLogger.utils.factories.FormatterFactory;
 import org.jetbrains.annotations.NotNull;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        @NotNull Logger logger = new Logger(Main.class, new ConsoleHandler(new SimpleFormatter()));
+        // Initialize Console Logger
+        @NotNull Logger<Main> consoleLogger = new Logger<>(Main.class, new ConsoleHandler(FormatterFactory.FormatterType.SIMPLE));
 
-        logger.error("hello world");
+        // Log messages to console
+        consoleLogger.info("This is an info message.");
+        consoleLogger.debug("This is a debug message.");
+        consoleLogger.warn("This is a warning message.");
+        consoleLogger.error("This is an error message.");
+
+        // Initialize File Logger
+        @NotNull Logger<Main> fileLogger = new Logger<>(Main.class, new FileHandler(FormatterFactory.FormatterType.JSON, "src/main/java/com/codes/vinis/vLogger/logs.txt"));
+
+        // Log messages to a file
+        fileLogger.info("Info logged to file.");
+        fileLogger.error("Error logged to file.");
     }
 }
